f879e749bad44edba03979d43d09362426b2c726
Worawat12322 <71426736+Worawat12322@users.noreply.github.com>
2021-03-11 18:04:35 +0700

    update star

--- star.cpp
+++ star.cpp
@@ -1,16 +1,42 @@
 #include<stdio.h>
-int check(int *a){
-	while(*a%2!=1)
-	{
-		*a-=1;
-	}
-	return *a;
-}
 int main(){
-	int n;
+	int n,x,i,j,cout=0;
 	scanf("%d",&n);
-	check(&n);
-	printf("%d",n);
+	if(n%2==0)
+	{
+		x=n-1;
+	}
+	else x=n;
+	for(i=1;i<=(n+1)/2;i++,cout++)
+	{
+		if(i==1)
+		{
+			for(j=1;j<=x/2;j++)
+			{
+				printf("_");
+			}
+			printf("*");
+			for(j=1;j<=x/2;j++)
+			{
+				printf("_");
+			}
+			printf("\n");
+		}
+		else for(j=1;j<=x;j++)
+		{
+			if(j==((x+1)/2)-cout)
+			{
+				printf("*");
+			}
+			else printf("_");
+			if(j==((x+1)/2)+cout)
+			{
+				printf("*");
+			}
+			else printf("_");
+		}
+		printf("\n");	
+	}
 	return 0;
 }
 
