f3f83647e5c54f2d9dedfd870214ae38d80a31d2
Worawat12322 <71426736+Worawat12322@users.noreply.github.com>
2021-03-11 15:57:19 +0700

    Update uglt number.cpp

--- uglt number.cpp
+++ uglt number.cpp
@@ -1,12 +1,32 @@
 #include<stdio.h>
 int main(){
-	int n,i,div[3]={2,3,5},test,a;
+	int n,i,div[3]={2,3,5},di_n=0,test,a;
 	scanf("%d",&n);
 	int ans[n]={1};
 	if(n==1){
 		printf("1");
 	}
 	else {
+	
+	for(i=2;i<=n;i++)
+	{
+		test=ans[i-1]+1;
+		while(1)
+		{
+			if(test%2==0)
+			{
+				ans[i]=test;
+				break;
+			}
+			else if(test%3==0||test%5==0)
+			{
+				ans[i]=test;
+				break;
+			}
+			test++;
+		}
 	}
+}
+	printf("%d",ans[])
 	return 0;
 }
